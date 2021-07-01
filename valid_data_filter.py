3f9a1eca379098d1604220e66f0847e46568f730
Young Woo Nam <spow2544@gmail.com>
2021-07-01 12:34:36 +0900

    commit

--- valid_data_filter.py
+++ valid_data_filter.py
@@ -11,12 +11,17 @@ csv_2 = pd.read_csv('./data_2/HP_2011_20160701_filterd.csv', index_col=0)
 csv_3 = pd.read_csv('./data_2/HP_20160702_20201231_filterd.csv', index_col=0)
 
 data = pd.concat([csv_1, csv_2, csv_3])
+data = data.reset_index()
+data = data.drop('index', axis='columns')
+# %%
+data
 # %%
 #제외기준 2 위수술이력 문진 == 1
 data = data[data['SURGERY_STOMACH#138'] != 1]
-data
+
 # %% 내시경 STG, TG 포함
 csv_7 = data.copy()
+# map_fn: 검사결과내용#7의 값이 string이 아닌 Row를 제거
 csv_7['검사결과내용#7_process'] = csv_7['검사결과내용#7'].map(lambda x: map_fn(x))
 csv_7 = csv_7[csv_7['검사결과내용#7_process'] != 0]
 
@@ -31,6 +36,8 @@ p = re.compile('TG')
 csv_7['검사결과내용#7_process'] = csv_7['검사결과내용#7_process'].map(lambda x: find_text(x, p))
 csv_7 = csv_7[csv_7['검사결과내용#7_process'] != 0]
 indexs = list(set(index + list(csv_7.index)))
+
+data = data[data.index.isin(indexs)]
 # %%
 #제외기준 3 가족중 위장암존재
 data = data[data['FAMILY_CANCER_STOMACH_F#170'] != 1]
--- valid_data_select.py
+++ valid_data_select.py
@@ -3,12 +3,13 @@ import re
 import numpy as np
 import datatable
 import pandas as pd
+from utils import *
 pd.set_option('display.max_columns', None)
 # %%
 csv = datatable.fread('./data_2/HP_20160702_20201231_merged.csv').to_pandas()
-csv.head()
-# %%
-csv['검사결과내용#15'].value_counts()
+
+csv
+
 # %%
 
 # 검사결과내용#15 (HP 진단문 -> HP유무 판정)
@@ -25,14 +26,14 @@ else:
     print('something wrong')
 csv_15
 # %%
-csv['검사결과내용#20'].value_counts()
+# csv['검사결과내용#20'].value_counts()
 # %%
 # 검사결과내용#20에서 positive인 경우만 추출
 csv['검사결과내용#20_process'] = csv['검사결과내용#20'].map(lambda x: map_fn(x))
 csv_20 = csv[csv['검사결과내용#20_process'] != '']
 csv_20 = csv_20[csv_20['검사결과내용#20_process'] != 0]
 csv_20['result'] = csv_20['검사결과내용#20'].map(lambda x: find_text(x))
-csv_20 = csv_20[csv_20['result'] == 1]
+csv_20 = csv_20[csv_20['result'] == 1] # positive -> 1 negative -> 0
 csv_20
 # %%
 csv_24 = csv[csv['건강검진결과코드#24'] == 'G020']
@@ -46,26 +47,3 @@ data
 # %%
 data = data.drop(['C0', '검사결과내용#15_process', '검사결과내용#20_process'], axis='columns')
 data.to_csv('./data_2/HP_20160702_20201231_filterd.csv')
-
-# %%
-data
-# %%
-#제외기준 2 위수술이력 문진 == 1
-print(data[data['SURGERY_STOMACH#138'] != 1].value_counts())
-data = data[data['SURGERY_STOMACH#138'] != 1]
-data
-# %%
-#제외기준 3 가족중 위장암존재
-data = data[data['FAMILY_CANCER_STOMACH_F#170'] != 1]
-data = data[data['FAMILY_CANCER_STOMACH_M#171'] != 1]
-data = data[data['FAMILY_CANCER_STOMACH_SIB#172'] != 1]
-data = data[data['FAMILY_CANCER_STOMACH_CH#173'] != 1]
-data
-
-# %%
-data = data[~data['처방일자{연령}#4'].isna()]
-data = data[~data['성별#8'].isna()]
-data
-# %%
-print(data['검사결과내용#7'][1])
-# %%
